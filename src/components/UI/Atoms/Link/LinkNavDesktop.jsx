19be9380dd7fc7164b2f78406a33a1ea5e409a3b
pepetgoodboy <iqbaltw127@gmail.com>
2024-09-30 12:15:40 +0700

    feature: add dynamic navbar

--- src/components/UI/Atoms/Link/LinkNavDesktop.jsx
+++ src/components/UI/Atoms/Link/LinkNavDesktop.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function LinkNavDesktop({ link, name }) {
+  const location = useLocation();
+
+  const isActive = location.pathname === link;
+
   return (
-    <>
-      <Link
-        to={link}
-        className="border-transparent text-gray-500 hover:border-[#FFD800] hover:text-gray-700 border-b-2 py-2 mt-1"
-      >
-        {name}
-      </Link>
-    </>
+    <Link
+      to={link}
+      className={`text-gray-500 hover:border-[#FFD800] hover:text-gray-700 border-b-2 py-2 mt-1 ${
+        isActive ? "border-[#FFD800] text-gray-700" : "border-transparent"
+      }`}
+    >
+      {name}
+    </Link>
   );
 }
